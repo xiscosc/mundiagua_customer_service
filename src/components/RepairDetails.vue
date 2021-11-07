109df69e56150cb11e1ddf833d9a9eae16360846
Francesc Sastre Cabot <francesc.sastrecabot@showpad.com>
2021-11-07 16:14:34 +0100

    add type uppercase

--- src/components/RepairDetails.vue
+++ src/components/RepairDetails.vue
@@ -3,7 +3,7 @@
       <div class="col">
         <h5 class="repair-title-section">Detalles de la reparación</h5>
         <p class="repair-data">Identificador {{ repair.private_id }}</p>
-        <p class="repair-data">Reparación {{ repair.type }}</p>
+        <p class="repair-data">Reparación {{ repair.type.toUpperCase() }}</p>
         <p class="repair-data">Modelo {{ repair.model }}</p>
         <p class="repair-data">{{ repair.warranty | warrantySpanish }} Garantía</p>
         <p class="repair-data">Entrada el {{ repair.date | formatDate }}</p>
